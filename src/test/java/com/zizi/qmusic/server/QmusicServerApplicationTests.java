dbbb4188867b6435bbe3b8524c9dbec5257a5f4a
fanhuajun659 <357724264@qq.com>
2020-01-13 11:42:16 +0800

    测试接口可调用

---
+++ src/test/java/com/zizi/qmusic/server/QmusicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zizi.qmusic.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmusicServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
